d7196a73f8f0b37848b26f967bd5faf50aa40fd3
Piotr Grzesik <pj.grzesik@gmail.com>
2022-05-27 13:48:22 +0200

    feat: Finalize `utils-aws` package

--- utils-aws/src/get-credential-provider.js
+++ utils-aws/src/get-credential-provider.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const fromNodeProviderChain = require('./utils/from-node-provider-chain');
+const ensureAwsSpecificEnvironmentVariables = require('./ensure-aws-specific-environment-variables');
 
 const getCredentialProvider = ({ profile, region } = {}) => {
+  ensureAwsSpecificEnvironmentVariables();
+
   return fromNodeProviderChain({
     profile,
     // Config below applies only to STS client configuration
--- utils-aws/src/index.js
+++ utils-aws/src/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const getCredentialProvider = require('./get-credential-provider');
+const ensureAwsSpecificEnvironmentVariables = require('./ensure-aws-specific-environment-variables');
+
+module.exports = {
+  getCredentialProvider,
+  ensureAwsSpecificEnvironmentVariables,
+};
