86b713f79a33de289a7110a16fe00da61aef5246
Piotr Grzesik <pj.grzesik@gmail.com>
2022-05-30 14:43:44 +0200

    refactor: Introduce `getRegion`

--- utils-aws/src/index.js
+++ utils-aws/src/index.js
@@ -2,8 +2,10 @@
 
 const getCredentialProvider = require('./get-credential-provider');
 const ensureAwsSpecificEnvironmentVariables = require('./ensure-aws-specific-environment-variables');
+const getRegion = require('./get-region');
 
 module.exports = {
   getCredentialProvider,
+  getRegion,
   ensureAwsSpecificEnvironmentVariables,
 };
