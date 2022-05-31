ce5c2f8b71406dac25eb2279da67b8047bf1f59a
Matthieu Napoli <matthieu@mnapoli.fr>
2022-05-31 10:33:05 +0200

    fix: Fix and expand CDK tests

--- core-aws/test/Cdk.test.ts
+++ core-aws/test/Cdk.test.ts
@@ -1,12 +1,21 @@
 import { ComponentContext } from '@serverless-components/core';
 import proxyquire from 'proxyquire';
-import { SinonSpy, spy } from 'sinon';
+import { SinonStub, stub } from 'sinon';
 import { App, Stack } from 'aws-cdk-lib';
 import { expect } from 'chai';
+import { mockClient } from 'aws-sdk-client-mock';
+import { GetCallerIdentityCommand, STSClient } from '@aws-sdk/client-sts';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Cdk } from '../src';
 import FakeComponentContext from './FakeComponentContext';
 
+const stsMock = mockClient(STSClient);
+
 describe('Cdk', () => {
+  beforeEach(() => {
+    stsMock.reset();
+  });
+
   it('bootstraps the AWS CDK automatically', async () => {
     const [cdk, spawnStub] = mockCdk(new FakeComponentContext());
 
@@ -17,6 +26,27 @@ describe('Cdk', () => {
     expect(spawnStub.callCount).to.be.greaterThanOrEqual(1);
     expect(spawnStub.getCall(0).args[0]).to.equal('cdk');
     expect(spawnStub.getCall(0).args[1][0]).to.equal('bootstrap');
+  });
+
+  it('does not bootstraps twice', async () => {
+    const [cdk, spawnStub] = mockCdk(new FakeComponentContext());
+
+    const app1 = new App();
+    new Stack(app1, 'stack-name');
+    await cdk.deploy(app1);
+    // Deploy again, but with a different stack
+    const app2 = new App();
+    const stack = new Stack(app2, 'stack-name');
+    new Bucket(stack, 'bucket');
+    await cdk.deploy(app2);
+
+    expect(spawnStub.callCount).to.equal(3);
+    expect(spawnStub.getCall(0).args[0]).to.equal('cdk');
+    expect(spawnStub.getCall(0).args[1][0]).to.equal('bootstrap');
+    expect(spawnStub.getCall(1).args[0]).to.equal('cdk');
+    expect(spawnStub.getCall(1).args[1][0]).to.equal('deploy');
+    expect(spawnStub.getCall(2).args[0]).to.equal('cdk');
+    expect(spawnStub.getCall(2).args[1][0]).to.equal('deploy');
   });
 
   it('does not bootstrap the AWS CDK in parallel to avoid race conditions', async () => {
@@ -56,6 +86,24 @@ describe('Cdk', () => {
     expect(spawnStubB.getCall(0).args[0]).to.equal('cdk');
     expect(spawnStubB.getCall(0).args[1][0]).to.equal('bootstrap');
   });
+
+  it('skips deploying unchanged stacks', async () => {
+    const [cdk, spawnStub] = mockCdk(new FakeComponentContext());
+
+    const app1 = new App();
+    new Stack(app1, 'stack-name');
+    await cdk.deploy(app1);
+    // Deploy the same stack again
+    const app2 = new App();
+    new Stack(app2, 'stack-name');
+    await cdk.deploy(app2);
+
+    expect(spawnStub.callCount).to.equal(2);
+    expect(spawnStub.getCall(0).args[0]).to.equal('cdk');
+    expect(spawnStub.getCall(0).args[1][0]).to.equal('bootstrap');
+    expect(spawnStub.getCall(1).args[0]).to.equal('cdk');
+    expect(spawnStub.getCall(1).args[1][0]).to.equal('deploy');
+  });
 });
 
 /**
@@ -65,8 +113,8 @@ describe('Cdk', () => {
 function mockCdk(
   componentContext: ComponentContext,
   onSpawn?: (command: string, args: string[]) => number | Promise<number>
-): [Cdk, SinonSpy] {
-  const spawnStub = spy((command: string, args: string[]) => {
+): [Cdk, SinonStub] {
+  const spawnStub = stub().callsFake((command: string, args: string[]) => {
     return {
       on: async (arg, cb) => {
         if (arg === 'close') {
@@ -79,6 +127,9 @@ function mockCdk(
           if (arg === 'data') cb('This is the CDK CLI output');
         },
       },
+      kill: () => {
+        // Nothing to kill
+      },
     };
   });
   const MockedCdk = proxyquire('../src/Cdk', {
@@ -88,6 +139,11 @@ function mockCdk(
   const cdk = new MockedCdk(componentContext, 'stack-name', 'us-east-1', {
     region: 'us-east-1',
   }) as Cdk;
+
+  stsMock.on(GetCallerIdentityCommand).resolves({
+    // Fake AWS account ID
+    Account: '1234567890',
+  });
 
   return [cdk, spawnStub];
 }
